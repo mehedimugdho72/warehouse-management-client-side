00293e7fe363265f15063895582fe79d911839de
rana135 <rana286090@gmail.com>
2022-05-09 05:11:05 +0600

    some issue solve

--- src/Pages/Home/ServiceDetails/ServiceDetails.js
+++ src/Pages/Home/ServiceDetails/ServiceDetails.js
@@ -97,70 +97,17 @@ const ServiceDetails = () => {
                         </div>
 
                     </div>
-                    {/* <div data-aos="zoom-in-down">
-                        <div className='text-center mt-5 rounded-3
-                col-lg-10 col-md-10 col-sm-12 col-12 mx-auto'>
-                            <div className='delivered'>
-                                <div data-aos="zoom-in">
-                                    <div className='fs-1 text-white'>
-                                        Do you really want to Delivered it?
-                                    </div>
-                                </div>
-                                <h2 className='text-center mt-3 text-white'>Product name: {product.name}</h2>
-                                <h2 className='text-center mt-3 text-white'>Quantity: {product.quantity}</h2>
-                                <hr className='text-white' style={{ height: '3px' }} />
-                                <div className="d-flex justify-content-center">
-                                    <Button variant="info" className='btn-lg mb-3 fw-bolder'>
-                                        Delivered
-                                    </Button>
-                                </div>
-                            </div>
-                        </div>
-                    </div> */}
                 </div>
-                {/* <div data-aos="zoom-in-down">
-                    <div className='text-center rounded-3 mb-5 mt-5
-                col-lg-10 col-md-10 col-sm-12 col-12 mx-auto'>
-                        <form onSubmit={handleUpdateUsers}
-                            className='quantityAdded'>
-                            <h1>Quantity:- {product.quantity}</h1>
-                            <input type="number" name="quantity" className='mt-1 text-center' style={{ height: "50px", width: "17rem", borderRadius: "10px" }} placeholder="Quantity Added" />
-                            <hr className='text-white' style={{ height: '3px' }} />
-                            <input className='mb-3 fw-bolder bg-info rounded-3' type="submit" value="Restock Inventory" />
+
+                
+                <div className='d-flex  mt-3 '>
+                        <button onClick={() => handleUpdate()} className='update-btn ms-2'>Delivered</button>
+                        <form className='d-flex w-50 ms-auto ' onSubmit={handleSubmit(onSubmit)}>
+
+                            <input className='mb-3 mr-2' placeholder='Enter Price' type="number" {...register("Quantity")} />
+                            <input className='update-btn' type="submit" value="Update" />
                         </form>
                     </div>
-                </div> */}
-
-                <div className='d-flex  mt-3 '>
-                    <div>
-                        <div data-aos="zoom-in-down">
-                            <div className='text-center mt-5 rounded-3
-                col-lg-10 col-md-10 col-sm-12 col-12 mx-auto'>
-                                <div className='delivered'>
-                                    <div data-aos="zoom-in">
-                                        <div className='fs-1 text-white'>
-                                            Do you really want to Delivered it?
-                                        </div>
-                                    </div>
-                                    <h2 className='text-center mt-3 text-white'>Product name: {product.name}</h2>
-                                    <h2 className='text-center mt-3 text-white'>Quantity: {product.quantity}</h2>
-                                    <hr className='text-white' style={{ height: '3px' }} />
-                                    <div className="d-flex justify-content-center">
-                                        <Button onClick={() => handleUpdate()} variant="info" className='btn-lg mb-3 fw-bolder'>
-                                            Delivered
-                                        </Button>
-                                    </div>
-                                </div>
-                            </div>
-                        </div>
-                    </div>
-                    
-                    <form className='d-flex w-50 ms-auto ' onSubmit={handleSubmit(onSubmit)}>
-
-                        <input className='mb-3 mr-2' placeholder='Enter Price' type="number" {...register("quantity")} />
-                        <input className='update-btn' type="submit" value="Update" />
-                    </form>
-                </div>
             </div>
         </div>
     );
